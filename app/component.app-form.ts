565c469c7644bc0ee76ee4bc04ab440e8918ed96
Juan Carlos Coral <govindasd@gmail.com>
2018-05-19 12:30:14 -0300

    Pegando valores dos select onChange e colocando no objeto chamado answers
    ,Estou usando id de cada select como key da propriedade do objeto a ser adicionada ao objeto no evento onChange.

--- app/component.app-form.ts
+++ app/component.app-form.ts
@@ -10,55 +10,27 @@ import { QuestionsService } from './questions.service';
 
 export class AppFormComponent implements OnInit {
 	questions = [];
-
+	answers = {};
+	
 	constructor(private _questionsService: QuestionsService){}
 	ngOnInit(){
 		this._questionsService.getQuestions()
 		.subscribe(
 			(resQuestionsData) => {
-				/*let objArr = [];
-				objArr = Object.keys(resQuestionsData).map((key) => {return resQuestionsData[key]});
-				let currArrObj = objArr[0];
-				let objOfArr = 
-				
-				
-				Object.keys(currArrObj).map((key) =>{
-																		currArrObj[key].input_id = key
-																		return currArrObj[key]
-																	}
-															);  este era meu código para arranjar tudo*/
-				/*
-					Produz:
-					[
-						{	"label":"Você curte hamburguer?",
-							"hidden":false,
-							"options":{
-								"yes":{"label":"Sim!","value":"1","show":"meat_spot"},
-								"no":{"label":"Não","value":"0","hide":"meat_spot"}
-							},
-							"input_id":"likes_hamburger"
-						},
-						{	"label":"Qual o ponto da carne que você prefere?",
-							"hidden":true,
-							"options":{
-								"rare":{"label":"Mal passada!","value":"rare"},
-								"medium":{"label":"Ao ponto","value":"medium"},
-								"well":{"label":"Bem passada","value":"well"}
-							},
-							"input_id":"meat_spot"
-						}
-					]
-				*/
 				this.questions = 	Object.keys(resQuestionsData.questions).map(key => {
 														let value = resQuestionsData.questions[key];
 														value.options = Object.keys(value.options).map(_key => value.options[_key]);
 														return Object.assign({ name: key }, value);
 													});
-				//this.questions = 
 			}, 
 			error => alert(error), 
 			() => console.log(JSON.stringify(this.questions))
 		)
 	}
-
+	addAnswer(event:any){
+		//console.log(event.target.value);
+		//console.log(event.target.id);
+		this.answers[event.target.id] = event.target.value;
+		console.log(JSON.stringify(this.answers));
+	}
 }
